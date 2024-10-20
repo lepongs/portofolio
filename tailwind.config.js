5ede5b70923ca4c683da9574c0c7ecfbaf230919
lepongs <leponging@gmail.com>
2024-10-20 19:58:47 +0800

    upload to repo

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/portofolio.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins']
+      },
+    },
+  },
+  plugins: [],
+}
+
